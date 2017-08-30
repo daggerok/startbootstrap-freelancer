5cda3d01ff6c2245fd717b12af858601700e11bd
Maksim Kostromin <daggerok@gmail.com>
2017-08-31 02:56:07 +0300

    Reforemat; Remove useless code; Update README

--- gulpfile.js
+++ gulpfile.js
@@ -18,94 +18,94 @@ var banner = ['/*!\n',
 ].join('');
 
 // Compiles SCSS files from /scss into /css
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   return gulp.src('scss/freelancer.scss')
-    .pipe(sass())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest('css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+             .pipe(sass())
+             .pipe(header(banner, {
+               pkg: pkg
+             }))
+             .pipe(gulp.dest('css'))
+             .pipe(browserSync.reload({
+               stream: true
+             }));
 });
 
 // Minify compiled CSS
-gulp.task('minify-css', ['sass'], function() {
+gulp.task('minify-css', ['sass'], function () {
   return gulp.src('css/freelancer.css')
-    .pipe(cleanCSS({
-      compatibility: 'ie8'
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+             .pipe(cleanCSS({
+               compatibility: 'ie8'
+             }))
+             .pipe(rename({
+               suffix: '.min'
+             }))
+             .pipe(gulp.dest('css'))
+             .pipe(browserSync.reload({
+               stream: true
+             }));
 });
 
 // Minify custom JS
-gulp.task('minify-js', function() {
+gulp.task('minify-js', function () {
   return gulp.src('js/freelancer.js')
-    .pipe(uglify())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('js'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+             .pipe(uglify())
+             .pipe(header(banner, {
+               pkg: pkg
+             }))
+             .pipe(rename({
+               suffix: '.min'
+             }))
+             .pipe(gulp.dest('js'))
+             .pipe(browserSync.reload({
+               stream: true
+             }));
 });
 
 // Copy vendor files from /node_modules into /vendor
 // NOTE: requires `npm install` before running!
-gulp.task('copy', function() {
+gulp.task('copy', function () {
   gulp.src([
-      'node_modules/bootstrap/dist/**/*',
-      '!**/npm.js',
-      '!**/bootstrap-theme.*',
-      '!**/*.map'
-    ])
-    .pipe(gulp.dest('vendor/bootstrap'))
+    'node_modules/bootstrap/dist/**/*',
+    '!**/npm.js',
+    '!**/bootstrap-theme.*',
+    '!**/*.map'
+  ])
+      .pipe(gulp.dest('vendor/bootstrap'));
 
   gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-    .pipe(gulp.dest('vendor/jquery'))
+      .pipe(gulp.dest('vendor/jquery'));
 
   gulp.src(['node_modules/popper.js/dist/umd/popper.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-    .pipe(gulp.dest('vendor/popper'))
+      .pipe(gulp.dest('vendor/popper'));
 
   gulp.src(['node_modules/jquery.easing/*.js'])
-    .pipe(gulp.dest('vendor/jquery-easing'))
+      .pipe(gulp.dest('vendor/jquery-easing'));
 
   gulp.src([
-      'node_modules/font-awesome/**',
-      '!node_modules/font-awesome/**/*.map',
-      '!node_modules/font-awesome/.npmignore',
-      '!node_modules/font-awesome/*.txt',
-      '!node_modules/font-awesome/*.md',
-      '!node_modules/font-awesome/*.json'
-    ])
-    .pipe(gulp.dest('vendor/font-awesome'))
-})
+    'node_modules/font-awesome/**',
+    '!node_modules/font-awesome/**/*.map',
+    '!node_modules/font-awesome/.npmignore',
+    '!node_modules/font-awesome/*.txt',
+    '!node_modules/font-awesome/*.md',
+    '!node_modules/font-awesome/*.json'
+  ])
+      .pipe(gulp.dest('vendor/font-awesome'));
+});
 
 // Default task
 gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
   browserSync.init({
     server: {
       baseDir: ''
     },
-  })
-})
+  });
+});
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() {
+gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function () {
   gulp.watch('scss/*.scss', ['sass']);
   gulp.watch('css/*.css', ['minify-css']);
   gulp.watch('js/*.js', ['minify-js']);
